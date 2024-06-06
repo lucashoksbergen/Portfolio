5002e2f3c1f28a533094e979e7251f4aa150280b
Axisas <lucashoksbergen@gmail.com>
2024-06-06 17:37:01 +0200

    Removed projects that were not relevant

--- public/projecttext.js
+++ public/projecttext.js
@@ -1,28 +1,19 @@
 sessionStorage.setItem(
   "Underwater Cooperation",
-  `This was a project that I worked on during the second half of 2022-2023.
-   For this project I was the main programmer.
-    The goal of this project was to make a serious game to promote people with cooperating,
-     as well as teaching them how to do this in a natural manner.
-     <br><br>
-      The game is made using Unity and C#, it is also the first time
-       I experimented with adding local multiplayer to a game. 
-       <br><br>
-       The main gameplay-loop of the game is every player making
-        independent choices that affect general statistics negatively or positively,
-         with the goal of the game being to keep all of these balanced. 
-         The difficulty here arises in the fact that the players do not know what choices the others are making,
-          and have to discuss this, without saying their choices outright.`
-);
-
-sessionStorage.setItem(
-  "AirBar Planet Infographic",
-  "placeholdertext describing the project."
-);
-
-sessionStorage.setItem(
-  "Monster Hunter World:Iceborne Buildcrafting tool",
-  "placeholdertext describing the project."
+  `This was a project that I worked on during the second half of 2022-2023 with a team of 5 people.
+  For this project I was the programmer.
+  The goal of this project was to make a serious game to promote people with cooperating,
+  as well as teaching them how to do this in a natural manner.
+  <br><br>
+  The game is made using Unity and C#, it is also the first time
+  I experimented with adding local multiplayer to a game. As is visible in the demo below, 
+  one of the features of the game is that every decision you make dynamically updates on everyone's version of the game. 
+  <br><br>
+  The main gameplay-loop of the game is every player making
+  independent choices that affect general statistics negatively or positively,
+  with the goal of the game being to keep all of these balanced. 
+  The difficulty here arises in the fact that the players do not know what choices the others are making,
+  and have to discuss this, without mentioning their choices outright.`
 );
 
 sessionStorage.setItem(
@@ -41,25 +32,14 @@ sessionStorage.setItem(
 );
 
 sessionStorage.setItem(
-  "Project Mafia",
+  "AirBar Planet Infographic",
   "placeholdertext describing the project."
 );
 
 sessionStorage.setItem(
-  "Unity stealth AI tech test",
+  "Monster Hunter World:Iceborne Buildcrafting tool",
   "placeholdertext describing the project."
 );
-
-sessionStorage.setItem(
-  "Plastic Disaster",
-  "placeholdertext describing the project."
-);
-
-sessionStorage.setItem(
-  "Arachnophobia Simulator",
-  "placeholdertext describing the project."
-);
-
 sessionStorage.setItem(
   "Op Jezelf App",
   "placeholdertext describing the project."
