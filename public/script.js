99724f5130dac4f99d3bf337692a3a1a13efa627
Axisas <lucashoksbergen@gmail.com>
2024-06-07 14:43:28 +0200

    Added logos with links, also bunch of css updates

--- public/script.js
+++ public/script.js
@@ -1,19 +1,20 @@
 const grid = document.querySelector(`.gridwrapper`);
+const title = document.querySelector(`#title`);
 
 function clickGridItem() {
   // toggle popupwindow
   grid.style.display = "none";
   popupWindow.style.display = "flex";
   popupWindowClose.style.display = "block";
+  title.style.display = "block";
+
   for (const child of popupWindow.children) {
     if (child.tagName == "IMG") {
       // replaces the image with an image with the same name as the project chosen.
       child.src = `imgs/${this.innerHTML}.png`;
     }
     if (child.tagName == "P") {
-      child.innerHTML =
-        `${this.innerHTML} <br><br>` +
-        sessionStorage.getItem(`${this.innerHTML}`);
+      child.innerHTML = sessionStorage.getItem(`${this.innerHTML}`);
     }
 
     if (child.tagName == "VIDEO") {
@@ -25,6 +26,8 @@ function clickGridItem() {
 function closePopup() {
   popupWindow.style.display = "none";
   popupWindowClose.style.display = "none";
+  title.style.display = "none";
+
   grid.style.display = "flex";
 }
 
@@ -33,7 +36,7 @@ const popupWindow = document.getElementById("popup");
 const popupWindowClose = document.getElementById("closepopup");
 
 // adds a click eventlistener to all the projects that are visible in the grid on the projects page.
-const gridItems = Array.from(document.getElementsByClassName("grid"));
+const gridItems = Array.from(document.getElementsByClassName("gridproject"));
 if (gridItems) {
   gridItems.forEach((element) =>
     element.addEventListener("click", clickGridItem)
